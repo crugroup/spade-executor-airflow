d02a9f6573ac20e768345e824e28845bcc47aeb4
Zakariah Nuccio <zak.nuccio@crugroup.com>
2024-01-29 20:04:36 +0000

    add verify_ssh to history
    move missing env var error to inside function

--- spade_executor_airflow/history_provider.py
+++ spade_executor_airflow/history_provider.py
@@ -13,18 +13,21 @@ class AirflowRunHistoryProvider(HistoryProvider):
     airflow_url = os.environ.get("SPADE_AIRFLOW_URL")
     airflow_username = os.environ.get("SPADE_AIRFLOW_USERNAME")
     airflow_password = os.environ.get("SPADE_AIRFLOW_PASSWORD")
-    if airflow_url is None or airflow_username is None or airflow_password is None:
-        logger.error("Airflow URL, username, or password not set")
+    airflow_verify_ssl = os.environ.get("SPADE_AIRFLOW_VERIFY_SSL", "true").lower() == "true"
 
     @classmethod
     def get_runs(cls, process: Process, request, *args, **kwargs):
         """Trigger a DAG to run."""
+
+        if cls.airflow_url is None or cls.airflow_username is None or cls.airflow_password is None:
+            raise ValueError("Airflow URL, username, or password not set")
 
         logger.info(f"Retrieving Airflow runs for DAG ID {process.system_params['dag_id']}")
         auth_key = base64.b64encode(f"{cls.airflow_username}:{cls.airflow_password}".encode()).decode()
         resp = requests.get(
             f"{cls.airflow_url}/api/v1/dags/{process.system_params['dag_id']}/dagRuns",
             headers={"Authorization": f"Basic {auth_key}"},
+            verify=cls.airflow_verify_ssl,
         )
         if resp.status_code != 200:
             logger.error(f"Failed to get DAG runs: {resp.text}")
