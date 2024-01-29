dcca304b21f64a07dd915fda685f57c40d1daa70
Marcin Szymański <ms32035@gmail.com>
2024-01-29 21:57:38 +0200

    add option to disable ssl verify

--- spade_executor_airflow/executor.py
+++ spade_executor_airflow/executor.py
@@ -12,6 +12,7 @@ class AirflowRunDAGExecutor(Executor):
     airflow_url = os.environ.get("SPADE_AIRFLOW_URL")
     airflow_username = os.environ.get("SPADE_AIRFLOW_USERNAME")
     airflow_password = os.environ.get("SPADE_AIRFLOW_PASSWORD")
+    airflow_verify_ssl = os.environ.get("SPADE_AIRFLOW_VERIFY_SSL", "true").lower() == "true"
 
     @classmethod
     def run(cls, process, user_params) -> RunResult:
@@ -23,13 +24,14 @@ class AirflowRunDAGExecutor(Executor):
         logger.info(f"Running Airflow DAG ID {process.system_params['dag_id']}")
         auth_key = base64.b64encode(f"{cls.airflow_username}:{cls.airflow_password}".encode()).decode()
         resp = requests.post(
-            f"{cls.airflow_url}/api/v1/dags/{process.system_params['dag_id']}/dag_runs",
+            f"{cls.airflow_url}/api/v1/dags/{process.system_params['dag_id']}/dagRuns",
             headers={
                 "Authorization": f"Basic {auth_key}",
             },
             json={
-                "conf": user_params["conf"],
+                "conf": user_params or {},
             },
+            verify=cls.airflow_verify_ssl,
         )
         if resp.status_code != 200:
             logger.error(f"Failed to run DAG: {resp.text}")
