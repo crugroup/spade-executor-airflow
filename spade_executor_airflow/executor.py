d1ae0fed8d62b9eebfe729bc66b834c6df8f9b4b
Marcin Szymański <ms32035@gmail.com>
2024-01-29 21:24:00 +0200

    update to the new api

--- spade_executor_airflow/executor.py
+++ spade_executor_airflow/executor.py
@@ -12,17 +12,18 @@ class AirflowRunDAGExecutor(Executor):
     airflow_url = os.environ.get("SPADE_AIRFLOW_URL")
     airflow_username = os.environ.get("SPADE_AIRFLOW_USERNAME")
     airflow_password = os.environ.get("SPADE_AIRFLOW_PASSWORD")
-    if airflow_url is None or airflow_username is None or airflow_password is None:
-        logger.error("Airflow URL, username, or password not set")
-
+    
     @classmethod
-    def run(cls, system_params, user_params) -> RunResult:
+    def run(cls, process, user_params) -> RunResult:
         """Trigger a DAG to run."""
 
-        logger.info(f"Running Airflow DAG ID {system_params['dag_id']}")
+        if cls.airflow_url is None or cls.airflow_username is None or cls.airflow_password is None:
+            raise ValueError("Airflow URL, username, or password not set")
+
+        logger.info(f"Running Airflow DAG ID {process.system_params['dag_id']}")
         auth_key = base64.b64encode(f"{cls.airflow_username}:{cls.airflow_password}").decode()
         resp = requests.post(
-            f"{cls.airflow_url}/api/v1/dags/{system_params['dag_id']}/dag_runs",
+            f"{cls.airflow_url}/api/v1/dags/{process.system_params['dag_id']}/dag_runs",
             headers={
                 "Authorization": f"Basic {auth_key}",
             },
@@ -32,6 +33,6 @@ class AirflowRunDAGExecutor(Executor):
         )
         if resp.status_code != 200:
             logger.error(f"Failed to run DAG: {resp.text}")
-            return RunResult(result="failure")
+            return RunResult(process=process, status="error")
 
-        return RunResult(result="success")
+        return RunResult(process=process, status="running")
