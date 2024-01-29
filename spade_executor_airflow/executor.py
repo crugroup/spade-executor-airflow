a0efd08b7aa4e038eb0c0119573b0b8a413ad37e
Zakariah Nuccio <zak.nuccio@crugroup.com>
2024-01-29 19:33:20 +0000

    fix formatting

--- spade_executor_airflow/executor.py
+++ spade_executor_airflow/executor.py
@@ -12,7 +12,7 @@ class AirflowRunDAGExecutor(Executor):
     airflow_url = os.environ.get("SPADE_AIRFLOW_URL")
     airflow_username = os.environ.get("SPADE_AIRFLOW_USERNAME")
     airflow_password = os.environ.get("SPADE_AIRFLOW_PASSWORD")
-    
+
     @classmethod
     def run(cls, process, user_params) -> RunResult:
         """Trigger a DAG to run."""
@@ -21,9 +21,7 @@ class AirflowRunDAGExecutor(Executor):
             raise ValueError("Airflow URL, username, or password not set")
 
         logger.info(f"Running Airflow DAG ID {process.system_params['dag_id']}")
-        auth_key = base64.b64encode(
-            f"{cls.airflow_username}:{cls.airflow_password}".encode()
-    ).decode()
+        auth_key = base64.b64encode(f"{cls.airflow_username}:{cls.airflow_password}".encode()).decode()
         resp = requests.post(
             f"{cls.airflow_url}/api/v1/dags/{process.system_params['dag_id']}/dag_runs",
             headers={
