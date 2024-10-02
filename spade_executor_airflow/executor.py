62f7bd276d50388ad27ec4172a452bb9f3539acd
zak-nuccio <152978738+zak-nuccio@users.noreply.github.com>
2024-10-02 15:10:23 +0100

    Update spadesdk to 0.3.0 (#9)

--- spade_executor_airflow/executor.py
+++ spade_executor_airflow/executor.py
@@ -15,7 +15,7 @@ class AirflowRunDAGExecutor(Executor):
     airflow_verify_ssl = os.environ.get("SPADE_AIRFLOW_VERIFY_SSL", "true").lower() == "true"
 
     @classmethod
-    def run(cls, process, user_params, user_id) -> RunResult:
+    def run(cls, process, user_params, user, *args, **kwargs) -> RunResult:
         """Trigger a DAG to run."""
 
         if cls.airflow_url is None or cls.airflow_username is None or cls.airflow_password is None:
@@ -24,7 +24,8 @@ class AirflowRunDAGExecutor(Executor):
         logger.info(f"Running Airflow DAG ID {process.system_params['dag_id']}")
         auth_key = base64.b64encode(f"{cls.airflow_username}:{cls.airflow_password}".encode()).decode()
         params = user_params or {}
-        params["spade__user_id"] = user_id
+        params["spade__user_id"] = user.id
+        params["spade__user_email"] = user.email
         logger.info(f"Sending request to {cls.airflow_url}/api/v1/dags/{process.system_params['dag_id']}/dagRuns")
         logger.info(f"Params: {params}")
         resp = requests.post(
