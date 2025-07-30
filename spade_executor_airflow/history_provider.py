791fa0f5f08bd8a94d3072b9d75798bae70c27d8
ichirotakami <138510975+ichirotakami@users.noreply.github.com>
2025-07-30 14:57:13 +0100

    release --> main (#17)
    * dev --> uat (#12)
    
    * Airflow 3 support (#11)
    
    * Migrate authentication to airflow 3
    
    * update auth method to match endpoint renaming
    
    * fix getting token from airflow auth
    
    * update api urls
    
    * order historical dag runs by logical date
    
    * set logical date when executing dag
    
    * add timezone info to logical date input
    
    * dev --> release (#16)
    
    * Airflow 3 support (#11)
    
    * Migrate authentication to airflow 3
    
    * update auth method to match endpoint renaming
    
    * fix getting token from airflow auth
    
    * update api urls
    
    * order historical dag runs by logical date
    
    * set logical date when executing dag
    
    * add timezone info to logical date input
    
    * error for when checkbox not complete (#15)
    
    * handle multiple dags in execution logs (#14)
    
    * handle multiple dags in execution logs
    
    * sort
    
    * put outside
    
    * bump up
    
    ---------
    
    Co-authored-by: zak-nuccio <152978738+zak-nuccio@users.noreply.github.com>
    Co-authored-by: Zakariah Nuccio <zak.nuccio@crugroup.com>
    
    ---------
    
    Co-authored-by: zak-nuccio <152978738+zak-nuccio@users.noreply.github.com>
    Co-authored-by: Zakariah Nuccio <zak.nuccio@crugroup.com>

--- spade_executor_airflow/history_provider.py
+++ spade_executor_airflow/history_provider.py
@@ -24,46 +24,56 @@ class AirflowRunHistoryProvider(HistoryProvider):
         if cls.airflow_url is None or cls.airflow_username is None or cls.airflow_password is None:
             raise ValueError("Airflow URL, username, or password not set")
 
-        logger.info(f"Retrieving Airflow runs for DAG ID {process.system_params['dag_id']}")
         token = utils.request_airflow_token(
             cls.airflow_url,
             cls.airflow_username,
             cls.airflow_password,
             verify_ssl=cls.airflow_verify_ssl,
         )
-        resp = requests.get(
-            f"{cls.airflow_url}/api/v2/dags/{process.system_params['dag_id']}/dagRuns?order_by=-logical_date",
-            headers={"Authorization": f"Bearer {token}"},
-            verify=cls.airflow_verify_ssl,
-        )
-        if resp.status_code != 200:
-            logger.error(f"Failed to get DAG runs: {resp.text}")
-            return ()
-        data = resp.json()
-        ret = []
-        for run in data["dag_runs"]:
-            status = RunResult.Status.NEW
-            result = None
-            if run["state"] == "success":
-                status = RunResult.Status.FINISHED
-                result = RunResult.Result.SUCCESS
-            elif run["state"] == "failed":
-                status = RunResult.Status.FINISHED
-                result = RunResult.Result.FAILED
-            elif run["state"] == "running" or run["state"] == "restarting":
-                status = RunResult.Status.RUNNING
-                result = None
-            process_run = RunResult(
-                process=process,
-                output=run,
-                status=status,
-                result=result,
-                created_at=(
-                    datetime.strptime(run.get("start_date"), "%Y-%m-%dT%H:%M:%S.%f%z")
-                    if run.get("start_date")
-                    else None
-                ),
-                user_id=run["conf"].get("spade__user_id"),
+
+        dag_ids = []
+        if "dag_ids" in process.system_params:
+            dag_ids = process.system_params["dag_ids"]
+        elif "dag_id" in process.system_params:
+            dag_ids = [process.system_params["dag_id"]]
+
+        for dag_id in dag_ids:
+            logger.info(f"Retrieving Airflow runs for DAG ID {dag_id}")
+
+            resp = requests.get(
+                f"{cls.airflow_url}/api/v2/dags/{dag_id}/dagRuns?order_by=-logical_date",
+                headers={"Authorization": f"Bearer {token}"},
+                verify=cls.airflow_verify_ssl,
             )
-            ret.append(process_run)
+            if resp.status_code != 200:
+                logger.error(f"Failed to get DAG runs: {resp.text}")
+                return ()
+            data = resp.json()
+            ret = []
+            for run in data["dag_runs"]:
+                status = RunResult.Status.NEW
+                result = None
+                if run["state"] == "success":
+                    status = RunResult.Status.FINISHED
+                    result = RunResult.Result.SUCCESS
+                elif run["state"] == "failed":
+                    status = RunResult.Status.FINISHED
+                    result = RunResult.Result.FAILED
+                elif run["state"] == "running" or run["state"] == "restarting":
+                    status = RunResult.Status.RUNNING
+                    result = None
+                process_run = RunResult(
+                    process=process,
+                    output=run,
+                    status=status,
+                    result=result,
+                    created_at=(
+                        datetime.strptime(run.get("start_date"), "%Y-%m-%dT%H:%M:%S.%f%z")
+                        if run.get("start_date")
+                        else None
+                    ),
+                    user_id=run["conf"].get("spade__user_id"),
+                )
+                ret.append(process_run)
+        ret.sort(key=lambda r: r.created_at, reverse=True)
         return ret
